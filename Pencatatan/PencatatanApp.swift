c4324aa4d137097436588c86a29fde8ec0b41976
Reza Juliandri <rezajuliandri20@gmail.com>
2025-04-26 23:33:45 +0800

    Initial Commit

---
+++ Pencatatan/PencatatanApp.swift
@@ -0,0 +1,20 @@
+//
+//  PencatatanApp.swift
+//  Pencatatan
+//
+//  Created by Reza Juliandri on 26/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PencatatanApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ PencatatanTests/PencatatanTests.swift
@@ -0,0 +1,17 @@
+//
+//  PencatatanTests.swift
+//  PencatatanTests
+//
+//  Created by Reza Juliandri on 26/04/25.
+//
+
+import Testing
+@testable import Pencatatan
+
+struct PencatatanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
